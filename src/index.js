30c3fab1165882fe9272a6b2f5351501933efdaf
adityad85 <aditya85d@gmail.com>
2018-06-13 03:58:11 +0530

    WIP

--- src/index.js
+++ src/index.js
@@ -17,7 +17,11 @@
 
 
 function ackermann(input) {
-  return input;
+  const { m, n } = input;
+  if (m === 0) { return n + 1; }
+  if (m > 0 && n === 0) { return ackermann({ m: m - 1, n: 1 }); }
+  if (m > 0 && n > 0) { return ackermann({ m: m - 1, n: ackermann({ m, n: n - 1 }) }); }
+  return 0;
 }
 
 /* Q2 (*)
