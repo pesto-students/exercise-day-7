04218cd82b712f462c4c38a302a1be8d9c6191da
navneet <navneet.sahota@outlook.com>
2018-06-12 22:50:26 +0530

    navneet-segment-1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@
 Create an iterable without using generator function.
 See the tests for this function to get the spec.
 */
-function simpleIterable() {}
+function simpleIterable() {
+  let index = 1;
+  const iterable = {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      if (index <= 5) {
+        index += 1;
+        return { value: index - 1, done: false };
+      }
+      return { value: undefined, done: true };
+    },
+  };
+  return iterable;
+}
 
 /* 2 (*)
 Create an iterable using generator function.
