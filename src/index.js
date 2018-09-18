932ff237cc4910bb87b73757219d6f73a9b65a2f
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-19 00:04:31 +0530

    Fixed generatorIterable

--- src/index.js
+++ src/index.js
@@ -23,7 +23,11 @@ Create an iterable using generator function.
 It should have the same functionality as the one in question 1
 */
 function* generatorIterable() {
-  yield 'abc';
+  let i = 1;
+  while (i <= 5) {
+    yield i;
+    i += 1;
+  }
 }
 
 /* 3 (Q6 in tests)
